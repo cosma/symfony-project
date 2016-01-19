f6c983132d72ae5d9ddb3a5b3bb9e093b295ee0a
Cosmin Voicu <cosmin.voicu@ocono.de>
2016-01-19 02:57:36 +0100

    add circleci selenium

--- web/app_test.php
+++ web/app_test.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || strrpos(@$_SERVER['REMOTE_ADDR'], '10.0.', -strlen(@$_SERVER['REMOTE_ADDR'])) === false
-    && !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    && !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '172.17.42.1', '192.168.99.1')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
